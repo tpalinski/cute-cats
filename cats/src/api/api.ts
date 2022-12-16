66a3c7e195f1a355728db405db8c7f8fb0653139
tpalinski <tpalinski@icloud.com>
2022-12-16 21:59:26 +0100

    React project structure

---
+++ cats/src/api/api.ts
@@ -0,0 +1,12 @@
+const url = new URL('https://api.thecatapi.com/v1/images/search');
+
+export async function getRandomCat(): Promise<Object> {
+    let response = await fetch(url);
+
+    if(!response.ok){
+        throw new Error('Error while fetching data from the api')
+    }
+
+    let result = response.json()
+    return result;
+}
